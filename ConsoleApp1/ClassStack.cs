aca3ba3272f8ce84e48daec3ec733cd2034fe60b
Memorik <memorik28@gmail.com>
2020-09-09 12:09:33 +0300

    Add class Stack

---
+++ ConsoleApp1/ClassStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class ClassStack
+    {
+        Stack myStack = new Stack();
+        
+
+    }
+}
