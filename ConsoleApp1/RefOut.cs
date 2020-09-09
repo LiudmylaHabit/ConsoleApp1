fe4c5872303147ea1cb6b4ffb7d0de96c4adc526
Anna Demchuk <anidemchuk@gmail.com>
2020-09-09 12:20:45 +0300

    Creare RefOutClass

---
+++ ConsoleApp1/RefOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public  class RefOut
+    {
+        int a=0;
+        int b=0;
+        public void Squere(ref int a, ref int b)
+        {
+            a = 3;
+            b = 5;
+        }
+
+        public void Squere (int a, int b, out int squre)
+        {
+            squre = a * b;
+        }
+    }
+}
